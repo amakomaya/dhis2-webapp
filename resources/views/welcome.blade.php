45d51006d596eead2fa2abef07c2e39468c84c72
kripa-123 <shresthakritisha1@gmail.com>
2024-04-29 12:22:41 +0545

    changed logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DHIS 2</title>
-    <link rel="icon" href="{{ asset('dhis_logo.png') }}"> <!-- Replace "path/to/your/image.png" with the actual path to your image file -->
+    <title>DHIS 2 </title>
+    <link rel="icon" href="{{ asset('dhis_logo.png') }}"> 
+    <meta name="description" content="Discover a versatile data management solution for Nepal across diverse sectors with DHIS2. Sign up for free to set up a customized data management server, perfectly tailored to your organization's requirements." />
+    <meta name="keywords" content="DHIS2, Nepal, health, data" />
+    <meta name="author" content="Amakomaya"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
