869e0013feb57b89f5209280e6186db172c15d8f
kripa-123 <shresthakritisha1@gmail.com>
2024-04-29 14:41:18 +0545

    email feature added

--- resources/views/emails/data.blade.php
+++ resources/views/emails/data.blade.php
@@ -1,8 +1,11 @@
 <p>Dear Sir,</p>
 
-<p>Below is the information of client who wants to rehister in our dhis system</p>
+<p>Below is the information of client who wants to register in our dhis system</p>
 
 <p>Email: {{ $email }}</p>
 <p>Organization Type: {{ $organizationType }}</p>
 
+
+
+<p>Best regards,</p>
 <p>Amakomaya Apps Pvt Ltd.</p>
